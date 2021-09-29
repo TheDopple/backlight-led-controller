a32164ead35bdc2403a111e9eb20750f929c1997
Derek Dernbach <Derekdernbach@gmail.com>
2021-09-28 20:31:30 -0700

    Much cleaner now, still working, ser reset implemented.

--- ledController/threadController.py
+++ ledController/threadController.py
@@ -7,70 +7,95 @@ import time
 import d3dshot
 import atexit
 
-def main(serialPort, xSegs, ySegs, refreshRate):
-    print("Starting LED Controller...")
+class ledController:
+    def __init__(self, serialPort, xSegs, ySegs, refreshRate_s, baud=115200, resetSer_s=600):
+        self.xSegs = int(xSegs)
+        self.ySegs = int(ySegs)
+        self.refreshRate_s = int(refreshRate_s)
 
-    #Serial cxn to Arduino
-    resetSerialTimer = 600 # resetSerial cxn every 600s Not implemented yet
- 
-    d = d3dshot.create()
-    d.display = d.displays[0]   
+        self.d = d3dshot.create()
 
-    while True:
-        print("Connecting to serial...")
-        resetSerial = time.time()
-        ser = serial.Serial(port=serialPort, baudrate=115200)
-        pixelColorState = {}
+        self.serialPort = serialPort
+        self.baud = baud
+        self.resetSer_s = resetSer_s
+        self.resetTimer = time.time()
 
-        while ser.isOpen():
-            cycleTimeStart = time.time()
-            img = d.screenshot()
-            #print("SS time: {} ms".format(round(time.time() - cycleTimeStart, 2)))  #~.05s
+        self._connectSer()
 
-            #Use resize for color averaging
-            resizedImg = img.resize(size=(xSegs, ySegs)).load()
-            #print("RS time: {} ms".format(round(time.time() - cycleTimeStart, 2))) #~.01s
-
-            #Build serial data
-            serialData = []
-            serialData.append(60) #< to start serial data
-            for pixelIndex in range(xSegs):
-                r = resizedImg[pixelIndex,0][0]
-                g = resizedImg[pixelIndex,0][1]
-                b = resizedImg[pixelIndex,0][2]
-
-                if pixelColorState.get(pixelIndex, None) != [r,g,b]:
-                    serialData.append(pixelIndex)
-                    serialData.append(r) #1B Red value uint_8
-                    serialData.append(g) #1B Blue value uint_8
-                    serialData.append(b) #1B Green value uint_8
-                    pixelColorState.update({pixelIndex:[r,g,b]})
-                    print({pixelIndex:[r,g,b]})
-            serialData.append(62) #> to complete serial data
-
-            #Serial build time is negligible
-            #print("serialData build time: {} ms".format(round(time.time() - cycleTimeStart, 2)))
-
-            if len(serialData) > 2:
-                ser.write(bytes(serialData))
-                ser.reset_input_buffer() #Dump rx buffer.  We don't care about serial returns
-
-            print("Cycle runtime: {} ms".format(round(time.time() - cycleTimeStart, 2))) # .1 s           
-            time.sleep(1/refreshRate)
-
-            if (time.time() - resetSerial) >= resetSerialTimer:
-                print("Resetting serial connection")
-                ser.close()
+        self.pixelColorState = {}
     
+    def _connectSer(self):
+        self.ser = serial.Serial(port=self.serialPort, baudrate=int(self.baud))
+
+    def _resetSer(self):
+        print("Resetting Serial...")
+        self.ser.close()
+        time.sleep(.1)
+        self.resetTimer = time.time()
+        self._connectSer()
+
+    def _serialPayloadBuilder(self, img):
+        #Build serial data
+        serialData = []
+        serialData.append(60) #< to start serial data
+        for pixelIndex in range(self.xSegs):
+            r = img[pixelIndex,0][0] #1B Red value uint_8
+            g = img[pixelIndex,0][1] #1B Blue value uint_8
+            b = img[pixelIndex,0][2] #1B Green value uint_8
+
+            # Only add pixels with changed RGB values to serial payload
+            if self.pixelColorState.get(pixelIndex, None) != [r,g,b]:
+                serialData.append(pixelIndex)
+                serialData.append(r) 
+                serialData.append(g) 
+                serialData.append(b) 
+                self.pixelColorState.update({pixelIndex:[r,g,b]})
+        serialData.append(62) #> to complete serial data
+    
+        return serialData
+
+    def _takeCurrentScreenshot(self):
+        #Screenshots primary monitor, reduces pixels to xSegs,ySegs for color averaging
+        return self.d.screenshot().resize(size=(self.xSegs, self.ySegs)).load()
+
+    def start(self):
+        while self.ser.isOpen():
+            cycleTimeStart = time.time()
+
+            serialData = self._serialPayloadBuilder(self._takeCurrentScreenshot())
+            if len(serialData) > 2:
+                self.ser.write(bytes(serialData))
+                self.ser.reset_input_buffer()
+
+            print("Cycle runtime: {} ms".format(round(time.time() - cycleTimeStart, 2)))
+            if time.time() - self.resetTimer >= self.resetSer_s: self._resetSer()
+            time.sleep(1/self.refreshRate_s)  #Let's not blow up serial
+
+    #needs fixin
+    #@atexit.register 
+    def _exit(self):
+        #Set all LEDs to 0,0,0 and close serial cxn
+        pixelClearPld = []
+        pixelClearPld.append(60)
+        for i in range(0,xSegs):
+            pixelClearPld.append(i)
+            pixelClearPld.append(0)
+            pixelClearPld.append(0)
+            pixelClearPld.append(0)
+        pixelClearPld.append(62)
+        self.ser.write(bytes(pixelClearPld))
+        self.ser.close()
+
 if __name__=="__main__":
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('serialPort', help="Serial port")
     parser.add_argument('--xSegs', '-x', default=30, help='X LED count.')
     parser.add_argument('--ySegs', '-y', default=8, help='Y screen segment count. Used for sampling resolution.')
-    parser.add_argument('--refreshRate', '-z', default=240, help='Refresh rate Hz')
-    #parser.add_argument('--redBias', '-r', default=16, help='Red RGB value bias') #Maybe?
+    parser.add_argument('--refreshRate', '-z', default=240, help='Refresh rate Hz') #2x monitor Hz cuz why not
+    #parser.add_argument('--redBias', '-r', default=16, help='Red RGB value bias')
     #parser.add_argument('--greenBias', '-g', default=16, help='Green RGB value bias')
     #parser.add_argument('--blueBias', '-b', default=120, help='Blue RGB value bias')
+    #parser.add_argument('--brightnessBias', '-l', default=120, help='Blue RGB value bias')
     args = parser.parse_args()
     
-    main(args.serialPort, int(args.xSegs), int(args.ySegs), int(args.refreshRate))
+    l = ledController(args.serialPort, int(args.xSegs), int(args.ySegs), int(args.refreshRate)).start()
